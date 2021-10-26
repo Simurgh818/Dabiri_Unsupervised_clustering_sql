0ae961c84cca37b1d73a27e7bfae35cad56eb731
Sina Dabiri <sina.dabiri@berkeley.edu>
2021-10-26 12:52:11 -0400

    found creatine. need to get their values next.

--- acute_kidney_injury.sql
+++ acute_kidney_injury.sql
@@ -33,22 +33,24 @@ WITH first_admission_time AS
 			END AS age_group
 	FROM first_admission_time
 )
-select g.subject_id, g.age_group, c.icd9_code, e.itemid, d.label
+select g.subject_id, g.age_group, c.icd9_code, l.itemid, d.label
 from age g
 	INNER JOIN diagnoses_icd c
 	ON g.subject_id = c.subject_id
 
 -- 3: find serum creatinine and urine output info about them
-	INNER JOIN chartevents AS e
-	ON c.subject_id = e.subject_id
+	INNER JOIN labevents AS l
+	ON c.subject_id = l.subject_id
 	
-	INNER JOIN D_ITEMS as d
-	ON e.itemid = d.itemid 
+	INNER JOIN d_labitems as d
+	ON l.itemid = d.itemid 
 -- 2: Select the adult and older adult group, diagnosed for acute kidney injury icd9_code = '5849'
-	WHERE (age_group='adult' or age_group='>89') and icd9_code = '5849'
+	WHERE (age_group='adult' or age_group='>89') and icd9_code = '5849' and 
+	(d.label ='Creatine Kinase (CK)' or d.label = 'Creatine Kinase, MB Isoenzyme' or
+	d.fluid = 'Urine') 
 -- 	and e.itemid = 'Urine'
 
-	GROUP BY g.subject_id, g.age_group, c.icd9_code, e.itemid, d.label
+	GROUP BY g.subject_id, g.age_group, c.icd9_code, l.itemid, d.label
 	-- d.label
 	ORDER BY g.subject_id
 	
