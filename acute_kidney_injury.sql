2b1695c956820d6bf7b5b957f04cb066b1f618dd
Sina Dabiri <sina.dabiri@berkeley.edu>
2021-10-26 12:35:46 -0400

    drafting the python script for the clustering.

--- acute_kidney_injury.sql
+++ acute_kidney_injury.sql
@@ -33,18 +33,23 @@ WITH first_admission_time AS
 			END AS age_group
 	FROM first_admission_time
 )
-select g.subject_id, g.age_group, c.icd9_code, l.itemid
+select g.subject_id, g.age_group, c.icd9_code, e.itemid, d.label
 from age g
 	INNER JOIN diagnoses_icd c
 	ON g.subject_id = c.subject_id
 
 -- 3: find serum creatinine and urine output info about them
-	INNER JOIN labevents AS l
-	ON c.subject_id = l.subject_id
--- 2: Select the adult and older adult group, diagnosed for acute kidney injury
-	WHERE (age_group='adult' or age_group='>89') and icd9_code = '5849' and l.itemid = '82461'
+	INNER JOIN chartevents AS e
+	ON c.subject_id = e.subject_id
+	
+	INNER JOIN D_ITEMS as d
+	ON e.itemid = d.itemid 
+-- 2: Select the adult and older adult group, diagnosed for acute kidney injury icd9_code = '5849'
+	WHERE (age_group='adult' or age_group='>89') and icd9_code = '5849'
+-- 	and e.itemid = 'Urine'
 
-	GROUP BY g.subject_id, g.age_group, c.icd9_code, l.itemid
+	GROUP BY g.subject_id, g.age_group, c.icd9_code, e.itemid, d.label
+	-- d.label
 	ORDER BY g.subject_id
 	
 -- 4: unsupervised clustering
